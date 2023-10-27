5c507171dbaafe205ab2650ae069224bb3f674bd
jeandtx <jean.doutriaux@gmail.com>
2023-10-27 11:58:50 +0200

    mfcc: adding computed songs to the dataset

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ from models.attributes import get_recommendations_attributes
 from models.mfcc import get_recommendations_mfcc
 from models.lyrics import get_recommendations_lyrics
 import pandas as pd
+import time
 
 def merged_model(song_title, song_artist, songs=pd.read_csv('data/more data.csv'), number_of_songs=2):
-
+    start_time = time.time()
     dataset_attributes = get_recommendations_attributes(song_title, song_artist, songs, number_of_songs*10)
+    step1_time = time.time()
+    print(f"Step 1 (Attributes) Execution Time: {step1_time - start_time:.2f} seconds")
 
     dataset_att_mfcc = get_recommendations_mfcc(song_title, song_artist, dataset_attributes, number_of_songs*5)
+    step2_time = time.time()
+    print(f"Step 2 (MFCC) Execution Time: {step2_time - step1_time:.2f} seconds")
 
     dataset_att_mfcc_lyrics = get_recommendations_lyrics(song_title, song_artist, dataset_att_mfcc, number_of_songs)
+
+    end_time = time.time()
+    print(f"Step 3 (Lyrics) Execution Time: {end_time - step2_time:.2f} seconds")
+    print(f"Total Execution Time: {end_time - start_time:.2f} seconds")
 
     return dataset_att_mfcc_lyrics
--- models/mfcc.py
+++ models/mfcc.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from models.functions import get_closest_songs, get_song_index, add_song
+import os
 
 def extract_features_from_mp3(mp3_link, max_seq_len=None):
     audio = requests.get(mp3_link)
@@ -38,12 +39,35 @@ def compute_pca(dataframe, max_seq_len=None):
     pca_df = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2', 'PC3'])
     return pca_df
 
+def load_pca_from_file(file_path):
+    try:
+        pca_df = pd.read_csv(file_path)
+        return pca_df
+    except FileNotFoundError:
+        return None
+
+def update_pca(song_data, pca_file_path):
+    pca_df = load_pca_from_file(pca_file_path)
+    
+    if pca_df is None:
+        pca_df = compute_pca(song_data)
+    else:
+        new_songs = song_data[~song_data.index.isin(pca_df.index)]
+        if not new_songs.empty:
+            new_pca_df = compute_pca(new_songs)
+            pca_df = pd.concat([pca_df, new_pca_df])
+    
+    pca_df.to_csv(pca_file_path, index=False)
+    return pca_df
+
 def get_recommendations_mfcc(song_name, song_artist, songs=pd.read_csv('./data/more data.csv'), number_of_songs=10):
     songs.dropna(subset=['Audio'], inplace=True)
     song_index = get_song_index(song_name, song_artist, songs)
     if song_index is None:
+        print('Song not found. Adding song to dataset. Computing PCA.')
+        if os.path.exists('./data/pca_mfcc.csv'):
+            os.remove('./data/pca_mfcc.csv')
         songs = add_song(song_name, song_artist, songs)
-        song_index = get_song_index(song_name, song_artist, songs)
-    pca_df = compute_pca(songs) # todo: try to fetch pca_df from csv
-    closest_songs = get_closest_songs(song_index, pca_df, songs, number_of_songs)
+    pca_df = update_pca(songs, './data/pca_mfcc.csv')
+    closest_songs = get_closest_songs(get_song_index(song_name, song_artist, pd.read_csv('./data/more data.csv')), pca_df, songs, number_of_songs)
     return closest_songs
