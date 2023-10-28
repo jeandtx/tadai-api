46f51b546a49ebd567f236f83f2e83fcce0f9fdb
jeandtx <jean.doutriaux@gmail.com>
2023-10-28 19:20:45 +0200

    index bug resolved

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import time
 
 def merged_model(song_title, song_artist, songs=pd.read_csv('data/more data.csv'), number_of_songs=2):
     start_time = time.time()
-    dataset_attributes = get_recommendations_attributes(song_title, song_artist, songs, number_of_songs*10)
+    dataset_attributes = get_recommendations_attributes(song_title, song_artist, songs, number_of_songs*10).reset_index(drop=True)
     step1_time = time.time()
     print(f"Step 1 (Attributes) Execution Time: {step1_time - start_time:.2f} seconds")
 
-    dataset_att_mfcc = get_recommendations_mfcc(song_title, song_artist, dataset_attributes, number_of_songs*5)
+    dataset_att_mfcc = get_recommendations_mfcc(song_title, song_artist, dataset_attributes, number_of_songs*5).reset_index(drop=True)
     step2_time = time.time()
     print(f"Step 2 (MFCC) Execution Time: {step2_time - step1_time:.2f} seconds")
 
-    dataset_att_mfcc_lyrics = get_recommendations_lyrics(song_title, song_artist, dataset_att_mfcc, number_of_songs)
+    dataset_att_mfcc_lyrics = get_recommendations_lyrics(song_title, song_artist, dataset_att_mfcc, number_of_songs).reset_index(drop=True)
 
     end_time = time.time()
     print(f"Step 3 (Lyrics) Execution Time: {end_time - step2_time:.2f} seconds")
